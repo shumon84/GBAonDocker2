db8c096b6ee5b830a559335bea76ba801523b61c
shumon84 <cameremon84@gmail.com>
2019-09-11 16:22:45 +0900

    wip

--- multitask/include/font.h
+++ multitask/include/font.h
@@ -3,13 +3,13 @@
 
 //======================================================================
 //
-//	font, 256x128@8, 
-//	+ palette 16 entries, not compressed
-//	+ 512 tiles not compressed
-//	Total size: 32 + 32768 = 32800
+//	font, 128x48@8, 
+//	+ palette 256 entries, not compressed
+//	+ 96 tiles not compressed
+//	Total size: 512 + 6144 = 6656
 //
-//	Time-stamp: 2019-09-03, 08:59:03
-//	Exported by Cearn's GBA Image Transmogrifier, v0.8.13
+//	Time-stamp: 2019-09-10, 01:10:36
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
 //	( http://www.coranac.com/projects/#grit )
 //
 //======================================================================
@@ -17,11 +17,11 @@
 #ifndef GRIT_FONT_H
 #define GRIT_FONT_H
 
-#define fontTilesLen 32768
-extern const unsigned short fontTiles[16384];
+#define fontTilesLen 6144
+extern const unsigned short fontTiles[3072];
 
-#define fontPalLen 32
-extern const unsigned short fontPal[16];
+#define fontPalLen 512
+extern const unsigned short fontPal[256];
 
 #endif // GRIT_FONT_H
 
